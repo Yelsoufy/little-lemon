115079022fc7a40172e572bfa3a5ee44013833c9
Yelsoufy <youssefelsoufy123@gmail.com>
2023-10-02 16:17:45 +0300

    Added components and assets will be used in the projject

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        {/* Add other navigation links */}
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
